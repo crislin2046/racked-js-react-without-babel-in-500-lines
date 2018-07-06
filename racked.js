523d7904def1e0a7d3ab67bb6b3de21691c37684
Cris @ DOSY <22254235+dosyago-coder-0@users.noreply.github.com>
2018-07-07 02:24:33 +0800

    ok

--- racked.js
+++ racked.js
@@ -79,253 +79,253 @@
   Object.assign(self,{save,load});
   Object.assign(self,{clone,descendent});
 
-	let debug = false;
+  let debug = false;
 
-	Object.assign(Racked,{render});
+  Object.assign(Racked,{render});
 
-	function FancyBorder(props) {
-		return R`
-			<div className=${'FancyBorder FancyBorder-' + props.color}>
-				${props.children}
-			</div>
-		`;
-	}
+  function FancyBorder(props) {
+    return R`
+      <div className=${'FancyBorder FancyBorder-' + props.color}>
+        ${props.children}
+      </div>
+    `;
+  }
 
-	function Dialog(props) {
-		return R`
-			<FancyBorder color="blue">
-				<h1 className="Dialog-title">
-					${props.title}
-				</h1>
-				<p className="Dialog-message">
-					${props.message}
-				</p>
-				${props.children}
-			</FancyBorder>
-		`;
-	}
+  function Dialog(props) {
+    return R`
+      <FancyBorder color="blue">
+        <h1 className="Dialog-title">
+          ${props.title}
+        </h1>
+        <p className="Dialog-message">
+          ${props.message}
+        </p>
+        ${props.children}
+      </FancyBorder>
+    `;
+  }
 
-	class SignUpDialog extends Racked.Component {
-		constructor(props) {
-			super(props);
-			this.handleChange = this.handleChange.bind(this);
-			this.handleSignUp = this.handleSignUp.bind(this);
-			this.state = {login: ''};
-		}
+  class SignUpDialog extends Racked.Component {
+    constructor(props) {
+      super(props);
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSignUp = this.handleSignUp.bind(this);
+      this.state = {login: ''};
+    }
 
-		render() {
-			return R`
-				<Dialog title="Mars Exploration Program"
-								message="How should we refer to you?">
-					<input value=${this.state.login}
-								 onChange=${this.handleChange} />
-					<button onClick=${this.handleSignUp}>
-						Sign Me Up!
-					</button>
-				</Dialog>
-			`;
-		}
+    render() {
+      return R`
+        <Dialog title="Mars Exploration Program"
+                message="How should we refer to you?">
+          <input value=${this.state.login}
+                 onChange=${this.handleChange} />
+          <button onClick=${this.handleSignUp}>
+            Sign Me Up!
+          </button>
+        </Dialog>
+      `;
+    }
 
-		handleChange(e) {
-			this.setState({login: e.target.value});
-		}
+    handleChange(e) {
+      this.setState({login: e.target.value});
+    }
 
-		handleSignUp() {
-			alert(`Welcome aboard, ${this.state.login}!`);
-		}
-	}
+    handleSignUp() {
+      alert(`Welcome aboard, ${this.state.login}!`);
+    }
+  }
 
-	Racked.render(
-		R`<SignUpDialog />`,
-		document.getElementById('root')
-	);
+  Racked.render(
+    R`<SignUpDialog />`,
+    document.getElementById('root')
+  );
 
-	function render(markup, where, memory = {roots:[], handlers:{}}) {
-		const {roots:roots = [], handlers:mhandlers={}} = memory;
-		Object.assign(memory,{roots,handlers:mhandlers});
-		let str, handlers;
-		({str:str=markup,handlers:handlers={}} = markup);
-		Object.assign(memory.handlers,handlers);
-		const rack = fc(str);
-		let isClass = false;
-		let component;
-		if ( ! rack ) {
-			return {str,handlers:memory.handlers};
-		}
+  function render(markup, where, memory = {roots:[], handlers:{}}) {
+    const {roots:roots = [], handlers:mhandlers={}} = memory;
+    Object.assign(memory,{roots,handlers:mhandlers});
+    let str, handlers;
+    ({str:str=markup,handlers:handlers={}} = markup);
+    Object.assign(memory.handlers,handlers);
+    const rack = fc(str);
+    let isClass = false;
+    let component;
+    if ( ! rack ) {
+      return {str,handlers:memory.handlers};
+    }
 
-		const parser = document.createTreeWalker(rack,NodeFilter.SHOW_ALL);
-		const stack = [];
-		let html = '';
+    const parser = document.createTreeWalker(rack,NodeFilter.SHOW_ALL);
+    const stack = [];
+    let html = '';
 
-		do {
-			const node = parser.currentNode;
-			switch( node.nodeType ) {
-				case Node.ELEMENT_NODE: {
-					const name = node.tagName.toLowerCase();
-					const CapitalizedNameIndex = str.toLowerCase().indexOf(name);
-					let CapitalizedName = name;
-					if ( CapitalizedNameIndex >= 0 ) {
-						CapitalizedName = str.substr(CapitalizedNameIndex,name.length);
-					}
-					// see if it's a ract component (if there's a function / class called <CapitalizedName>))
-					try {
-						const props = Array.from(node.attributes)
-							.reduce((all,{name,value}) => {
-								if ( name.startsWith('data-hid') && value.startsWith('hid:') ) {
-									if ( !! memory.handlers[value] ) {
-										memory.handlers[value].forEach( ({eventName,handler}) => {
-											const func = handler;
-											let name = eventName;
-											name = name.startsWith('on') ? name : ('on' + name
-												.replace(/^\w/, c => c.toUpperCase()));
-											all[name] = func;
-									 });
-									}
-								} else if ( name == "classname" ) {
-									node.setAttribute("class", value);
-									node.removeAttribute("classname");
-								} else {
-									try {
-										all[name] = load(value);
-									} catch(e) {
-										debug ? console.warn(e,name,value) : void 0;
-										all[name] = value;
-									}
-								}
-								return all;
-							},{});
-						isClass = eval(`descendent(${CapitalizedName},Racked.Component)`);
-						let componentRender, componentHtml;
-						Object.assign(props,memory.handlers.props);
-						let hasClosingTag = str.match(new RegExp(`</${CapitalizedName}`));
-						let cid;
-						if ( hasClosingTag ) {
-							cid = "cid:"+Math.random();
-							props.children = cid;
-							stack.push({localName:CapitalizedName,cid});
-						}
-						if ( isClass ) {
-							component = eval(`new ${CapitalizedName}(props)`);
-							componentRender = component.render(); 
-							({str:componentHtml=componentRender} = componentRender);
-						} else {
-							componentRender = eval(`${CapitalizedName}(props)`);
-							({str:componentHtml=componentRender} = componentRender);
-						}
-						Object.assign(memory.handlers, componentRender.handlers);
-						const renderedAgainComponentHtml = render({
-							str:componentHtml,
-							handlers:componentRender.handlers
-						},null,memory);
-						if ( componentHtml !== renderedAgainComponentHtml.str ) {
-							componentHtml = renderedAgainComponentHtml.str; 
-						}
-						if ( !! component ) {
-							const currentComponent = component;
-							const cfrag = df(componentHtml);
-							try {
-								const rid = cfrag.querySelector('[data-ractid]').dataset.ractid;
-								memory.roots.push(() => {
-									const root = document.querySelector(`[data-ractid="${rid}"]`);
-									currentComponent.root = root;
-									currentComponent.componentDidMount();
-								});
-							} catch(e){
-								debug ? console.warn(e) : void 0;
-								currentComponent.root = where;
-								currentComponent.noRactId = true;
-							}
-						}
-						if( hasClosingTag ) {
-							memory[cid] = componentHtml;
-						} else {
-							html += componentHtml;
-						}
-						break;
-					} catch(e) {
-						debug ? console.warn(e) : void 0;
-						// not a ract component so we need to close it if it's non-void
-						if ( ! VOID_ELEMENTS.has(node.localName) ) {
-							stack.push(node);
-						}
-						// and report it
-						const id = 'id:' + Math.random();
-						html += `<${name}${
-							node.childElementCount ? ` data-ractid="${id}"` : ''}${
-							node.attributes.length ? ' ' + Array.from(node.attributes)
-								.map( attr => `${attr.name}="${attr.value}"` )
-								.join(' ') : ''}>`;
-						// controlled form "select" new code
-							if ( name == 'select' && !! node.attributes.value ) {
-								const selected = node.attributes.value.value;
-								Array.from(node.querySelectorAll('*')).forEach( el => {
-									if ( el.localName == 'option' && el.value == selected ) {
-										el.setAttribute("selected","");
-									}
-								});
-						}
-						break;
-					}
-				}
-				default: {
-					if ( !! node.nodeValue ) {
-						const renderedAgainComponentHtml = render({str:node.nodeValue,handlers},null,memory);
-						if ( renderedAgainComponentHtml.str !== node.nodeValue ) {
-							html += renderedAgainComponentHtml.str;
-						} else {
-							html += node.nodeValue || '';
-						}
-					}
-					break;
-				}
-			}
-			if ( ! node.nextSibling && ! node.childNodes.length ) {
-				const parent = stack.pop(); 
-				if ( !! parent && parent.cid ) {
-					let rawHtml = memory[parent.cid];
-					rawHtml = rawHtml.replace(parent.cid,html);
-					html = rawHtml;
-				}
-				else if ( !! parent && ! VOID_ELEMENTS.has(parent.localName) ) {
-					html += `</${parent.localName}>`;
-				}
-			}
-		} while(parser.nextNode());
+    do {
+      const node = parser.currentNode;
+      switch( node.nodeType ) {
+        case Node.ELEMENT_NODE: {
+          const name = node.tagName.toLowerCase();
+          const CapitalizedNameIndex = str.toLowerCase().indexOf(name);
+          let CapitalizedName = name;
+          if ( CapitalizedNameIndex >= 0 ) {
+            CapitalizedName = str.substr(CapitalizedNameIndex,name.length);
+          }
+          // see if it's a ract component (if there's a function / class called <CapitalizedName>))
+          try {
+            const props = Array.from(node.attributes)
+              .reduce((all,{name,value}) => {
+                if ( name.startsWith('data-hid') && value.startsWith('hid:') ) {
+                  if ( !! memory.handlers[value] ) {
+                    memory.handlers[value].forEach( ({eventName,handler}) => {
+                      const func = handler;
+                      let name = eventName;
+                      name = name.startsWith('on') ? name : ('on' + name
+                        .replace(/^\w/, c => c.toUpperCase()));
+                      all[name] = func;
+                   });
+                  }
+                } else if ( name == "classname" ) {
+                  node.setAttribute("class", value);
+                  node.removeAttribute("classname");
+                } else {
+                  try {
+                    all[name] = load(value);
+                  } catch(e) {
+                    debug ? console.warn(e,name,value) : void 0;
+                    all[name] = value;
+                  }
+                }
+                return all;
+              },{});
+            isClass = eval(`descendent(${CapitalizedName},Racked.Component)`);
+            let componentRender, componentHtml;
+            Object.assign(props,memory.handlers.props);
+            let hasClosingTag = str.match(new RegExp(`</${CapitalizedName}`));
+            let cid;
+            if ( hasClosingTag ) {
+              cid = "cid:"+Math.random();
+              props.children = cid;
+              stack.push({localName:CapitalizedName,cid});
+            }
+            if ( isClass ) {
+              component = eval(`new ${CapitalizedName}(props)`);
+              componentRender = component.render(); 
+              ({str:componentHtml=componentRender} = componentRender);
+            } else {
+              componentRender = eval(`${CapitalizedName}(props)`);
+              ({str:componentHtml=componentRender} = componentRender);
+            }
+            Object.assign(memory.handlers, componentRender.handlers);
+            const renderedAgainComponentHtml = render({
+              str:componentHtml,
+              handlers:componentRender.handlers
+            },null,memory);
+            if ( componentHtml !== renderedAgainComponentHtml.str ) {
+              componentHtml = renderedAgainComponentHtml.str; 
+            }
+            if ( !! component ) {
+              const currentComponent = component;
+              const cfrag = df(componentHtml);
+              try {
+                const rid = cfrag.querySelector('[data-ractid]').dataset.ractid;
+                memory.roots.push(() => {
+                  const root = document.querySelector(`[data-ractid="${rid}"]`);
+                  currentComponent.root = root;
+                  currentComponent.componentDidMount();
+                });
+              } catch(e){
+                debug ? console.warn(e) : void 0;
+                currentComponent.root = where;
+                currentComponent.noRactId = true;
+              }
+            }
+            if( hasClosingTag ) {
+              memory[cid] = componentHtml;
+            } else {
+              html += componentHtml;
+            }
+            break;
+          } catch(e) {
+            debug ? console.warn(e) : void 0;
+            // not a ract component so we need to close it if it's non-void
+            if ( ! VOID_ELEMENTS.has(node.localName) ) {
+              stack.push(node);
+            }
+            // and report it
+            const id = 'id:' + Math.random();
+            html += `<${name}${
+              node.childElementCount ? ` data-ractid="${id}"` : ''}${
+              node.attributes.length ? ' ' + Array.from(node.attributes)
+                .map( attr => `${attr.name}="${attr.value}"` )
+                .join(' ') : ''}>`;
+            // controlled form "select" new code
+              if ( name == 'select' && !! node.attributes.value ) {
+                const selected = node.attributes.value.value;
+                Array.from(node.querySelectorAll('*')).forEach( el => {
+                  if ( el.localName == 'option' && el.value == selected ) {
+                    el.setAttribute("selected","");
+                  }
+                });
+            }
+            break;
+          }
+        }
+        default: {
+          if ( !! node.nodeValue ) {
+            const renderedAgainComponentHtml = render({str:node.nodeValue,handlers},null,memory);
+            if ( renderedAgainComponentHtml.str !== node.nodeValue ) {
+              html += renderedAgainComponentHtml.str;
+            } else {
+              html += node.nodeValue || '';
+            }
+          }
+          break;
+        }
+      }
+      if ( ! node.nextSibling && ! node.childNodes.length ) {
+        const parent = stack.pop(); 
+        if ( !! parent && parent.cid ) {
+          let rawHtml = memory[parent.cid];
+          rawHtml = rawHtml.replace(parent.cid,html);
+          html = rawHtml;
+        }
+        else if ( !! parent && ! VOID_ELEMENTS.has(parent.localName) ) {
+          html += `</${parent.localName}>`;
+        }
+      }
+    } while(parser.nextNode());
 
-		while(stack.length) {
-			let snode = stack.pop();
-			if ( !! snode ) {
-				if( ! VOID_ELEMENTS.has(parent.localName)) {
-					html += `</${parent.localName}>`;
-				}
-				snode = stack.pop(); 
-			}
-		}
+    while(stack.length) {
+      let snode = stack.pop();
+      if ( !! snode ) {
+        if( ! VOID_ELEMENTS.has(parent.localName)) {
+          html += `</${parent.localName}>`;
+        }
+        snode = stack.pop(); 
+      }
+    }
 
-		if ( isClass ) {
-			component.componentDidMount();
-		}
-		let output;
-		if ( ! where ) {
-			output = {str:html,handlers};
-		} else {
-			where.innerHTML = html;
-			memory.roots.forEach(f => f());
-			Object.keys(memory.handlers).forEach( hid => {
-				const node = document.querySelector(`[data-hid="${hid.replace(':', ":")}"]`);
-				if ( !! node ) {
-					memory.handlers[hid].forEach( ({eventName, handler}) => {
-						node.addEventListener(eventName, handler);
+    if ( isClass ) {
+      component.componentDidMount();
+    }
+    let output;
+    if ( ! where ) {
+      output = {str:html,handlers};
+    } else {
+      where.innerHTML = html;
+      memory.roots.forEach(f => f());
+      Object.keys(memory.handlers).forEach( hid => {
+        const node = document.querySelector(`[data-hid="${hid.replace(':', ":")}"]`);
+        if ( !! node ) {
+          memory.handlers[hid].forEach( ({eventName, handler}) => {
+            node.addEventListener(eventName, handler);
             if ( isClass ) {
               component.props[eventName] = handler;
             }
-					});
-				}
-			});
-			output = {str:html,handlers};
-		}
-		return output;
-	}
+          });
+        }
+      });
+      output = {str:html,handlers};
+    }
+    return output;
+  }
 
   function df( t ) {
     return (new DOMParser).parseFromString(`<template>${t}</template>`,"text/html").head.firstElementChild.content;
